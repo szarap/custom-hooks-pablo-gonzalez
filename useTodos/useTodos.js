5ce392513a5b78a25eee7beff8bb4bb029401886
Pablo Gonzalez <lic.szgonzalez@gmail.com>
2022-12-30 16:57:05 -0300

    useTodos updated

--- useTodos/useTodos.js
+++ useTodos/useTodos.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer } from 'react';
-import { todoReducer } from '../08-useReducer/todoReducer';
+import { todoReducer } from './todoReducer';
 
 const init = () => {
     return JSON.parse(localStorage.getItem('todos')) || [];
@@ -7,7 +7,7 @@ const init = () => {
 
 export const useTodos = () => {
 
-    const [ todos, dispatch ] = useReducer( todoReducer, [], init );
+    const [ todos, dispatc.( todoReducer, [], init );
 
     useEffect(() => {
         localStorage.setItem('todos', JSON.stringify( todos ) );
